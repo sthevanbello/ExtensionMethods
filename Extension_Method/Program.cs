4c59a7e5f6ea23b53481c31aace5fa436e824ccc
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-18 16:55:34 -0300

    Extension methods

---
+++ Extension_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Extension_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2021, 03, 18, 8, 10, 45);
+
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+
+            Console.WriteLine(s1.Cut(12));
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
